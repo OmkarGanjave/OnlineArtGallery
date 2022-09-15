081b11beef2164b254a11d714c890086f58a58f9
Sagar <ashwinimule2611.com>
2022-09-15 15:30:04 +0530

    Order Updated

--- Backend/Customer/src/main/java/com/example/demo/model/Customer.java
+++ Backend/Customer/src/main/java/com/example/demo/model/Customer.java
@@ -1,5 +1,7 @@
 package com.example.demo.model;
 
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,8 +9,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="customer")
@@ -27,13 +32,52 @@ public class Customer {
 	@Column
 	private String address;
 	@OneToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name="login_id")
+	@JoinColumn
 	User loginId;
 
+	@JsonIgnoreProperties("customer")
+	@OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+	//@Column - not associated
+	Set<Order> order;
+	
+	
 	public Customer() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	
+	
+
+	public Customer(int customerId, String firstName, String lastName, String emailId, String contactNo, String address,
+			User loginId, Set<Order> order) {
+		super();
+		this.customerId = customerId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+		this.contactNo = contactNo;
+		this.address = address;
+		this.loginId = loginId;
+		this.order = order;
+	}
+	
+	
+	
+
+
+	public Customer(String firstName, String lastName, String emailId, String contactNo, String address, User loginId,
+			Set<Order> order) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+		this.contactNo = contactNo;
+		this.address = address;
+		this.loginId = loginId;
+		this.order = order;
+	}
+
+	
 
 
 	public Customer(String firstName, String lastName, String emailId, String contactNo, String address, User loginId) {
@@ -47,24 +91,13 @@ public class Customer {
 	}
 
 
-	public Customer(int customerId, String firstName, String lastName, String emailId, String contactNo, String address,
-			User loginId) {
+
+	public Customer(int customerId) {
 		super();
 		this.customerId = customerId;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.emailId = emailId;
-		this.contactNo = contactNo;
-		this.address = address;
-		this.loginId = loginId;
 	}
 
-
-
-
-
-
-
+	
 	public String getFirstName() {
 		return firstName;
 	}
@@ -103,6 +136,22 @@ public class Customer {
 
 	public void setAddress(String address) {
 		this.address = address;
+	}
+
+
+
+	public Set<Order> getOrder() {
+		return order;
+	}
+
+
+
+	public void setOrder(Set<Order> order) {
+	for(Order o : order) 
+		{
+			o.setCustomer(this);;
+		}
+		this.order = order;
 	}
 
 	
--- Backend/Customer/src/main/java/com/example/demo/service/OrderService.java
+++ Backend/Customer/src/main/java/com/example/demo/service/OrderService.java
@@ -4,19 +4,46 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.model.Order;
 import com.example.demo.repository.OrderRepository;
 
 @Service
 public class OrderService {
-
 	@Autowired
-	OrderRepository or;
+	OrderRepository orderRepo;
+	
+	public int  addOrder(String customerId,int productQty,double totalPrice )
+	{
+		 List<Object[]> login=orderRepo.getLoginId(customerId);
+		 List<Object[]> customerID= orderRepo.getCustomerId((int)login.get(0)[0]);
+		 return orderRepo.addOrder((int)customerID.get(0)[0], productQty, totalPrice);
+		 
+	}
 	
 	public List<Order> getAll()
 	{
-		return or.findAll();
+		return orderRepo.findAll();
+	}
+	
+	public Order getOrderById(int orderId)
+	{
+		return  orderRepo.findById(orderId).get();
+	}
+	
+	public List<Order> searchOrder(String customerId)
+	{
+		List<Object[]> loginId = orderRepo.getLoginId(customerId);
+		
+		List<Object[]> customer=orderRepo.getCustomerId((int)loginId.get(0)[0]);
+		
+		return orderRepo.getOrderById((int)customer.get(0)[0]);
+	}
+	
+	public Order save(Order o)
+	{
+		return orderRepo.save(o);
 	}
 	
 }
