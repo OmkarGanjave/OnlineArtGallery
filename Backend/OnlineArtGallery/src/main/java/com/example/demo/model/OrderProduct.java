6c04ff60e3860d1305b6d8d510c6d4eab20a4622
Prajwalvp96 <patilpajju1996@gmail.com>
2022-09-19 11:11:11 +0530

    images

---
+++ Backend/OnlineArtGallery/src/main/java/com/example/demo/model/OrderProduct.java
@@ -0,0 +1,93 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="product")
+public class OrderProduct {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int productId;
+
+	@Column
+	private String productName;
+	
+	@Column
+	private String productDiscription;
+	
+	@Column
+	private double price;
+	
+	@Column
+	private int artistId;
+	
+	@Column
+	private int categoryId;
+
+	public OrderProduct() {
+		super();
+	}
+
+	public OrderProduct(int productId, String productName, double price) {
+		super();
+		this.productId = productId;
+		this.productName = productName;
+		this.price = price;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductDiscription() {
+		return productDiscription;
+	}
+
+	public void setProductDiscription(String productDiscription) {
+		this.productDiscription = productDiscription;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getArtistId() {
+		return artistId;
+	}
+
+	public void setArtistId(int artistId) {
+		this.artistId = artistId;
+	}
+
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	
+	
+}
