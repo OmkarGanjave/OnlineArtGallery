daf3ec4319b0417cc02ca216b91f4073d83ebe4a
Prajwalvp96 <patilpajju1996@gmail.com>
2022-09-19 17:56:36 +0530

    Backend Frontend

--- Backend/OnlineArtGallery/src/main/java/com/example/demo/repository/AddCartDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.AddCartDetails;
-
-public interface AddCartDetailsRepository extends JpaRepository<AddCartDetails, Integer> {
-
-}
---
+++ Frontend/onlineartgallary/src/Home/ImportImage.js
@@ -0,0 +1,10 @@
+export default function importAll(r) 
+{
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+}
+  
+  const images = importAll(require.context('F:/cdac2022/Project/Backend/OnlineArtGallery/images', false, /\.(png|jpe?g|svg)$/));
+  
+  <img src={images['2_3.jpg']} />
